6e1088d283f8f16e53eeeb51f7257b4f1dcd56bb
Lucas Dalcol Pereira <lucas.pereira_1@philips.com>
2019-08-19 00:10:32 -0300

    create project

---
+++ src/repositories/mentions-repository.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Mentions = mongoose.model('Mentions');
+
+exports.listMentions = async () => {
+  const res = await Mentions.find({}, 'friend mention -_id');
+  return res;
+};
+
+exports.createMention = async data => {
+  const mention = new Mentions(data);
+  await mention.save();
+};
+
+exports.updateMention = async (id, data) => {
+  await Mentions.findByIdAndUpdate(id, {
+    $set: data
+  });
+};
